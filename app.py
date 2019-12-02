1effdede35bf01e1b9ff0249a67a16e41c641f6c
sali <ucontacti2012@gmail.com>
2019-12-02 18:32:11 +0100

    more cleaning

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from flask import Flask, render_template, request, send_from_directory, abort
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "Modules/data"
 
+# TODO: add seesion handling
 
 def add_to_csv(down, up):
+   ### Merge down and up data together and add to time.csv
    with open('Modules/data/time.csv','a') as fd:
       for i in range(len(down)):
          fd.write(str(down[i]) + "," + str(up[i]))
@@ -16,12 +18,10 @@ def add_to_csv(down, up):
          else:
             fd.write("\n")
 
-@app.route("/")
-def index():
-   return render_template("index.html")
-
 @app.route("/post_data", methods = ['POST'])
 def get_post_data():
+   ### Add the key-stroke timings to database
+    print(request.remote_addr)
     js_d_data = request.form['down_time_data']
     js_u_data = request.form['up_time_data']
     add_to_csv(json.loads(js_d_data), json.loads(js_u_data))
@@ -29,33 +29,26 @@ def get_post_data():
 
 @app.route("/post_test", methods = ['POST'])
 def get_post_test():
+   ### Authenticate the test
     js_d_data = request.form['down_time_data']
     js_u_data = request.form['up_time_data']
     result = trainer(json.loads(js_d_data), json.loads(js_u_data))
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-# @app.route("/post_send_data", methods = ['POST'])
-# def get_post_send_data():
-#     result = save_data()
-#     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 @app.route('/download/<path:filename>', methods=['GET', 'POST'])
+   ### Download the time.csv
 def download(filename):
     uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
-    print(uploads)
-    print(filename)
-    return send_from_directory(directory=uploads, filename=filename)
+    try:
+        return send_from_directory(directory=uploads, filename=filename)
+    except FileNotFoundError:
+        abort(404)
 
-# @app.route("/get-csv/<csv_id>")
-# def get_csv(csv_id):
+@app.route("/")
+def index():
+   return render_template("index.html")
 
-#     filename = f"{csv_id}.csv"
-#     print(filename)
-#     print(app.config["CLIENT_CSV"])
-#     try:
-#         return send_from_directory(app.config["CLIENT_CSV"], filename=filename, as_attachment=True)
-#     except FileNotFoundError:
-#         abort(404)
 
 if __name__ == '__main__':
    app.run(host= '0.0.0.0', debug='on')
